d1db55fc62ba17b9adca166b5eca40a444904cdf
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-02-15 15:47:21 -0800

    added ItemTuple class to common

---
+++ src/common/ItemTuple.java
@@ -0,0 +1,34 @@
+package common;
+
+/**
+ * Holds an Item UPC and quantity for that Item.
+ * b/c we are using a singleton in the Inventory class,
+ * we only need to really pass around ItemTuple arrays
+ * to have all Item information, and customer cart info.
+ */
+public class ItemTuple {
+
+	private String UPC;
+	private int quantity;
+
+	public ItemTuple(String UPC, int quantity) {
+		this.UPC = UPC;
+		this.quantity = quantity;
+	}
+
+	public String getUPC() {
+		return UPC;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setUPC(String uPC) {
+		UPC = uPC;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
