76dda8b4accf9d16431cf022b300bc6a97609e87
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-02-15 20:37:12 -0800

    created Customer interface

--- src/common/Customer.java
+++ src/common/Customer.java
@@ -1,7 +1,18 @@
 package common;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface Customer extends Remote {
 
+    public void addItem(String UPC, int quantity) throws RemoteException;
+
+    public void addPayment(Payment payment) throws RemoteException;
+
+    public ArrayList<ItemTuple> getItems() throws RemoteException;
+
+    public Payment getPayment() throws RemoteException;
+
+    public String getName() throws RemoteException;
 }
