3b89734522bdb5507cf33e2cab2ea2b7c126d49c
Tenzin Wangchuk <tibwangchuk@gmail.com>
2015-02-16 08:28:02 -0800

    Updated PaymentType Interface

--- src/common/PaymentType.java
+++ src/common/PaymentType.java
@@ -14,9 +14,12 @@ public interface PaymentType extends Remote {
     }
 
     public void setPayType(PayType payType) throws RemoteException;
+    
+    public PayType getPayType() throws RemoteException;
 
     public String getCardNumber() throws RemoteException;
 
     public void setCardNumber(String cardNumber) throws RemoteException;
 
 }
+
