17e6891845143e71f39ceca52eb2ee1079048f8f
Tenzin Wangchuk <tibwangchuk@gmail.com>
2015-02-16 08:16:42 -0800

    PaymentType Interface Updated

--- src/common/PaymentType.java
+++ src/common/PaymentType.java
@@ -1,10 +1,22 @@
-package common;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * Accepted payment types
  */
-public enum PaymentType {
-	CASH,
-	CHECK,
-	CREDIT;
+public interface PaymentType extends Remote {
+
+    public enum PayType {
+
+        CASH,
+        CHECK,
+        CREDIT;
+    }
+
+    public void setPayType(PayType payType) throws RemoteException;
+
+    public String getCardNumber() throws RemoteException;
+
+    public void setCardNumber(String cardNumber) throws RemoteException;
+
 }
