9cc4c239f00bc22312df5740e671016512fd0fb8
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-02-17 12:07:50 -0800

    rename

--- src/common/Constants.java
+++ src/common/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final int REGISTRY_PORT = 1099;
 
-    public static final String POST_ID = "post";
+    public static final String STORE_ID = "store";
 
     public static final String REGISTRY_HOST = "localhost";
 }
