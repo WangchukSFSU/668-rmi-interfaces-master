aafa70db8bac48aa5d11a0be385aa1dc84b35540
Anthony Rodriguez <arodr101@mail.sfsu.edu>
2015-02-20 14:14:40 -0800

    string payment modifications

--- src/common/Customer.java
+++ src/common/Customer.java
@@ -1,32 +1,39 @@
 package common;
 
+import java.math.BigDecimal;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 /**
- * Object that corresponds to a user of this POST program.
- * Adds items to a list and uses post to process a sale.
+ * Object that corresponds to a user of this POST program. Adds items to a list
+ * and uses post to process a sale.
  */
 public interface Customer extends Remote {
 
     /**
      * Add each item and its quantity to the ArrayList of type ItemTuple
-     * @param UPC of the item
-     * @param quantity per item
+     * 
+     * @param UPC
+     *            of the item
+     * @param quantity
+     *            per item
      * @throws RemoteException
      */
     public void addItem(String UPC, int quantity) throws RemoteException;
 
     /**
      * Payment for purchase
-     * @param payment set for the customer
+     * 
+     * @param payment
+     *            set for the customer
      * @throws RemoteException
      */
     public void addPayment(Payment payment) throws RemoteException;
 
     /**
      * List of Customer Item added to ArrayList (Cart)
+     * 
      * @return ArrayList of Item from the Customer cart
      * @throws RemoteException
      */
@@ -38,6 +45,12 @@ public interface Customer extends Remote {
      */
     public Payment getPayment() throws RemoteException;
 
+    public PaymentType getPaymentType() throws RemoteException;
+
+    public BigDecimal getPaymentAmount() throws RemoteException;
+
+    public String getCardNumber() throws RemoteException;
+
     /**
      * @return The customer name
      * @throws RemoteException
@@ -46,7 +59,8 @@ public interface Customer extends Remote {
 
     /**
      * 
-     * @param name name of customer
+     * @param name
+     *            name of customer
      * @throws RemoteException
      */
     public void setName(String name) throws RemoteException;
